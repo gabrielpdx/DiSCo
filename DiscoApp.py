74bfe045033274e81bf79269ba1e564b60d7a70d
ga8e <rosati.gabriel@gmail.com>
2015-08-12 23:36:49 -0700

    Added some React

--- DiscoApp.py
+++ DiscoApp.py
@@ -1,11 +1,11 @@
 from flask import Flask
-from flask import render_template
+#from flask import render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return app.send_static_file('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
